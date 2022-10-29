e03a3affbccd323d8d751978bcf7f63520a92a37
lzy77_ <channelforletsplayshik@gmail.com>
2022-10-29 15:25:16 +0300

    Web Api ASP.NET

---
+++ Data/IServerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using innowise_task_server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace innowise_task_server.Data
+{
+    public interface IServerDbContext : IDisposable
+    {
+        DbSet<Fridge> Fridges { get; set; }
+
+        DbSet<Product> Products { get; set; }
+
+        DbSet<FridgeModel> FridgeModels { get; set; }
+
+        DbSet<FridgeProduct> FridgeProducts { get; set; }
+
+        int SaveChanges();
+    }
+}
---
+++ ServerModule.cs
@@ -0,0 +1,19 @@
+﻿using innowise_task_server.Data;
+using innowise_task_server.Models;
+using innowise_task_server.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace innowise_task_server
+{
+    public static class ServerModule
+    {
+        public static void AddServerModule(this IServiceCollection services)
+        {
+            var builder = WebApplication.CreateBuilder();
+            services.AddTransient<IFridgeService, FridgeService>();
+            services.AddTransient<IServerDbContext, ServerDbContext>();
+            services.AddDbContext<ServerDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Fridges.Data") ?? throw new InvalidOperationException("Connection string 'Fridges.Data' not found.")));
+        }
+    }
+}
